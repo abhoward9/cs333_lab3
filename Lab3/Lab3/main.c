9e156c73d043b47884272a227165dc6b5e264bda
abhoward9 <32381914+abhoward9@users.noreply.github.com>
2024-11-03 09:32:44 -0800

    format cleanup

---
+++ Lab3/Lab3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab3
+//
+//  Created by Andrew Howard on 31.10.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
